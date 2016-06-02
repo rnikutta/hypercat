cdaca33ff6af22141ae43003ccc3d01bed9b260f
Robert Nikutta <robert@pa.uky.edu>
2016-06-01 22:44:31 -0400

    Adding hypercat.py, ndiminterpolation_vectorized.py, and padarray.py (all stand-alone versions).

---
+++ hypercat.py
@@ -0,0 +1,327 @@
+import os
+import numpy as N
+import pyfits
+import h5py
+import ndiminterpolation_vectorized
+import padarray
+import logging
+logging.basicConfig(level=logging.INFO,format='%(levelname)s: %(message)s')
+
+
+__version__ = '20160601'   #yyymmdd
+__author__ = 'Robert Nikutta <robert.nikutta@gmail.com>'
+
+"""Utilities for handling CLUMPY image files."""
+
+
+class ModelCube:
+
+#    def __init__(self,hdffile='/home/robert/dev/hypercubizer/clumpy_img_cube_2200models_19waves_halfsized.hdf5',\
+    def __init__(self,hdffile='clumpy_img_cube_2200models_19waves_halfsized.hdf5',\
+                 hypercube='imgdata',\
+                 ndinterpolator=True):
+
+        logging.info("Opening HDF5 file: %s " % hdffile)
+        self.h = h5py.File(hdffile,'r')
+        self.group = self.h[hypercube]
+        
+        logging.info("Loading sampling parameters.")
+        shape = self.group['hypercubeshape'].value
+        select = N.argwhere(shape>1).flatten()  # selector mask: only theta_i with more than one element
+        
+        self.theta = (padarray.PadArray(self.group['theta'].value).unpad)
+        self.theta = [self.theta[j] for j in select]
+
+        self.paramnames = (self.group['paramnames'].value)[select]
+
+        if self.paramnames[-1] == 'wave':
+            self.x = self.theta[-3]
+            self.y = self.theta[-2]
+            self.wave = self.theta[-1]
+        else:
+            self.x = self.theta[-2]
+            self.y = self.theta[-1]
+
+        ramgigs = self.data = self.group['hypercube'].size*4/1024**3.
+        logging.info("Loading hypercube '%s' to RAM (~%.2f GB required) ..." % (hypercube,ramgigs))
+        self.data = self.group['hypercube'][...]
+        self.data = self.data.squeeze()  # drop from ndim-index all dimensions with length-one
+
+        if ndinterpolator is True:
+            logging.info("Instatiationg n-dim interpolation object ...")
+            self.ip = ndiminterpolation_vectorized.NdimInterpolation(self.data,self.theta,mode='lin')
+
+        logging.info("Done. Closing HDF5 file.")
+        self.h.close()
+
+
+    def pprinter(self):
+
+        """Very basic pretty-print of the parameter names and values."""
+        
+        import pprint
+
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(zip(self.paramnames,self.theta))
+
+        
+    def print_sampling(self,n=11,fmt="%7.3f"):
+
+        """Print a summary table of the sampled parameters and values.
+
+        Parameters:
+        -----------
+        n : int
+            n first elements of every parameter value list will be
+            printed. If the list is shorter than 'n', all of it will
+            be printed. Default: 11.
+
+        fmt : str
+            Format string to use for a single value. Default: '%7.3f'
+
+        """
+
+        maxstr = "%%% ds  " % max([len(p) for p in self.paramnames])  # longest parameter name
+        maxn = max([int(N.ceil(N.log10(t.size))) for t in self.theta])  # largest parameter cardinality
+
+        header = "Parameter Range                Nvalues  Values\n" + "-"*72
+        print header
+        
+        for p,v in zip(self.paramnames,self.theta):
+
+            srange = "[%s" % fmt % v[0] + " - %s]" % fmt % v[-1]  # range string
+            m = min(n,v.size)  # print n elements or all if number of elements smaller than n
+
+            vals = ["%s" % fmt % val for val in v[:m]]
+            svals = ",".join(vals)  # all values to be printed, as a single string
+            if v.size > n:
+                svals += ', ...'  # continuation indicator, if any
+
+            # bring everything together
+            print maxstr % p + "    %s" % srange + "  (%%%dd)   " % maxn % v.size +  svals
+
+
+    def get_image(self,vector,full=True):
+
+        # this is for the current array layout: ['sig', 'i', 'N', 'q', 'tv', 'x', 'y', 'wave']
+
+        vec = list(vector)
+        vec.insert(-1,tuple(self.x.tolist()))
+        vec.insert(-1,tuple(self.y.tolist()))
+        vec = tuple(vec)
+        
+        image = self.ip(vec)
+        image = image.squeeze()
+#        image = image.reshape((self.x.size,self.y.size))
+#        
+        if full is True:
+            image = mirror_halfimage(image)
+                
+        return image.squeeze()
+    
+            
+#    def get_image(self,theta,wave,full=False):
+#
+#        """
+#
+#        theta = [sig,i,N,q,tv]
+#        wave = [2.2,2.8,3.5], or [2.2,], etc.
+#
+#        """
+#
+#        try:
+#            theta = list(theta)
+#        except:
+#            raise
+#
+#        try:
+#            nwave = len(wave)
+#        except TypeError:
+#            wave = (wave,)
+#            nwave = 1
+#            assert (len(wave) == nwave), "Problem converting 'wave' to tuple."
+#
+#        image = N.zeros((self.x.size,self.y.size,nwave))
+#        for ix in xrange(self.x.size):
+#            for iy in xrange(self.y.size):
+#                image[ix,iy,:] = self.ip(N.array(theta+[ix,iy]),N.array(wave))
+#
+#        if full is True:
+#            image = mirror_halfimage(image)
+#                
+#        return image.squeeze()
+
+
+def mirror_halfimage(halfimage):
+
+    """Take half-sized image (image cube) and return the full version.
+
+    Parameters:
+    -----------
+    halfimage : array
+        Expected shape of halfimage: (nx,ny,nz), or (nx,ny),
+        and nx == ny/2 +1 must hold (or an exception will be raised).
+
+    Returns:
+    --------
+    fullimage : array
+        Array with first dimension mirrored, i.e. fullimage.shape =
+        (ny,ny,nz).
+
+    """
+        
+    shape = list(halfimage.shape)
+    nx, ny = shape[:2]
+
+    assert (nx == (ny/2 + 1)), "Image/cube doesn't seem to contain a half-sized array (dims = (%s). Not mirorring." % (','.join([str(s) for s in shape]))
+
+    shape[0] = 2*nx-1
+    
+    logging.info("Mirroring half-cube / half-image.")
+    
+    fullimage = N.zeros(shape,dtype=N.float32)
+    fullimage[:nx,...] = halfimage[::-1,...]
+    fullimage[nx-1:,...] = halfimage[:,...]
+    
+    return fullimage
+
+
+def get_clean_file_list(d,suffix='.fits'):
+
+    """Get a sorted list of file-paths ending in suffix, from directory d."""
+
+    sep = os.path.sep
+    files = os.listdir(d)
+    files = sorted([os.path.normpath(d+sep+f) for f in files if f.endswith(suffix)])
+
+    return files
+
+
+def get_sed_from_fitsfile(fitsfile):
+
+    """Compute SED from all image slices stored in fitsfile.
+
+    Computes for every image slice the area integral
+
+      \int I(x,y) dx dy = \Delta x \times \sum_j I_j
+
+    where j runs over all pixels of the image, and
+    \Delta x (= \Delta y) is the physical pixel size.
+
+    Parameters:
+    -----------
+    fitsfile : str
+        Name/path of a CLUMPY (full-sized) FITS file.
+
+    Returns:
+    --------
+    wave : array
+        Wavelength in micron.
+
+    sed : array
+        The SED lambda*F_lambda, area-integrated from the images.
+
+    """
+
+    header = pyfits.getheader(fitsfile)
+    wave = N.array([v for k,v in header.items() if k.startswith('LAMB')])  # in micron
+    data = pyfits.getdata(fitsfile)  # uses first HDU by default
+    sed = N.sum(data,axis=(1,2)) * header['CDELT1']**2   # \int I(x,y) dx dy
+
+    return wave, sed
+
+
+def mirror_fitsfile(fitsfile,hdus=('IMGDATA','CLDDATA'),save_backup=True):
+
+    """Mirror/flip a half-sized CLUMPY image cube (in fits file) along its x-axis.
+
+    CLUMPY image fits files come in two variants: full-sized (square
+    in x,y), and half-sized (reducing storage, b/c of the left-right
+    symmetry of the images).
+
+    This function expands all data cubes in a half-sized fits file to
+    full (square) size, and updates the fits file in-place. The
+    original file can be back-up automatically.
+
+    Parameters:
+    -----------
+    fitsfile : str
+        Name/path of a CLUMPY half-sized FITS file.
+
+    hdus : tuple
+        HDU names to expand. Default: ('IMGDATA','CLDDATA'). If one of
+        the requested HDUs does not seem to conform to the half-sized
+        type, a warning will be printed, and this HDU will be skipped.
+
+    save_backup : bool
+        If True (default), the original half-size fits file will be
+        backed up before the expanded (full-sized) fits file will be
+        written under the original name. The backed up file will have
+        the suffix '.bak'.
+
+    """
+
+    logging.info("Opening FITS file: %s " % fitsfile)
+
+    hdulist = pyfits.open(fitsfile,mode='update',save_backup=save_backup)
+
+    hdunames = [h.name for h in hdulist]
+    
+    for hduname in hdus:
+        
+        logging.info("Accessing HDU: %s" % hduname)
+        try:
+            hdu = hdulist[hduname]
+        except KeyError:
+            logging.warning("HDU '%s' in file %s not found. Skipping this HDU." % (hduname,fitsfile))
+            continue
+        
+        shape = list(hdu.shape)
+        ny, nx = shape[-2:]
+        if nx != (ny/2 + 1):   # caution: integer division
+            logging.warning("HDU '%s' in file %s doesn't seem to contain a half-sized array (dims = (%s). Skipping this HDU." % (hduname, fitsfile, ','.join([str(s) for s in shape])))
+            continue
+        else:
+            shape[-1] = 2*nx-1
+        
+        logging.info("Mirroring half-cube.")
+        aux = hdu.data[...,::-1]  # x-flipped half-cube
+        newdata = N.zeros(shape,dtype=N.float32)
+        newdata[...,:nx] = aux[...]
+        newdata[...,nx-1:] = hdu.data[...]
+        hdu.data = newdata
+
+    logging.info('Flushing and closing fits file.')
+    hdulist.close()
+    print
+            
+
+def mirror_all_fitsfiles(d,suffix='.fits',hdus=('IMGDATA','CLDDATA')):
+
+    """Mirror/flip all half-sized CLUMPY image cubes (in fits files) along their x-axis.
+
+    Please see docstring of mirror_fitsfile() for details.
+
+    Parameters:
+    -----------
+    d : str
+        Directory holding the files to be mirrored.
+
+    suffix : str
+        File ending of the fits files. Default: '.fits'
+
+    hdus : tuple
+        Sequence of HDU names to mirror/flip in every FITS file.
+
+    """
+    
+    files = get_clean_file_list(d,suffix='.fits')
+    nfiles = len(files)
+    
+    logging.info("Found %d '%s'-files in directory %s" % (nfiles,suffix,d))
+    
+    for j,f in enumerate(files):
+        logging.info("Working on file %s [%d/%d]" % (f,j+1,nfiles))
+        mirror_fitsfile(f,hdus=hdus)
+    
+    logging.info("All files mirrored.")
---
+++ ndiminterpolation_vectorized.py
@@ -0,0 +1,228 @@
+__author__ = "Robert Nikutta <robert.nikutta@gmail.com>"
+__version__ = '20150601'
+
+import numpy as N
+import warnings
+from scipy import interpolate, ndimage
+import itertools
+
+# Convert RuntimeWarnings, e.g. division by zero in some array elements, to Exceptions
+warnings.simplefilter('error', RuntimeWarning)
+
+
+class NdimInterpolation:
+
+    """N-dimensional interpolation on data hypercubes.
+
+    Operates on image(index) coordinates. Multi-linear or cubic-spline
+    (default).
+
+    """
+
+    def __init__(self,data,theta,order=1,mode='log'):
+
+        """Initialize an interpolator object.
+
+        Parameters
+        ----------
+        data : n-dim array or 1-d array
+            Model database to be interpolated. Sampled on a
+            rectilinear grid (it need not be regular!). 'data' is
+            either an n-dimensional array (hypercube), or a
+            1-dimensional array. If hypercube, each axis corresponds
+            to one of the model parameters, and the index location
+            along each axis grows with the parameter value. The last
+            axis is the 'wavelength' axis. If 'data' is a 1-d array of
+            values, it will be converted into the hypercube
+            format. This means that the order of entries in the 1-d
+            array must be as if constructed via looping over all axes,
+            i.e.
+
+              counter = 0
+              for j0 in theta[0]:
+                  for j1 in theta[1]:
+                     for j2 in theta[2]:
+                         ...
+                         hypercube[j0,j1,j2,...] = onedarray[counter]
+                         counter += 1
+
+        theta : list
+            List of 1-d arrays, each holding in ascending order the
+            unique values for one of the model parameters. The last
+            1-d array in theta is the wavelength array. Example: for
+            the CLUMPY models of AGN tori (Nenkova et al. 2008)
+
+              theta = [{i}, {tv}, {q}, {N0}, {sig}, {Y}, {wave}]
+
+            where the {.} are 1-d arrays of unique model parameter
+            values, e.g.
+
+              {i} = array([0,10,20,30,40,50,60,70,80,90]) (degrees).
+
+        order : int
+            Order of interpolation spline to be used. order=1
+            (default) is multi-linear interpolation, order=3 is
+            cubic-spline (quite a bit slower, and not necessarily
+            better, especially for complicated n-dim functions. order=1
+            is recommended.
+
+        mode : str
+            'log' is default, and will take log10(data) first, which
+            severely improves the interpolation accuracy if the data
+            span many orders of magnitude. This is of course only
+            applicable if all entries in 'data' are greater than
+            0. Any string other that 'log' will keep 'data' as-is.
+
+        Returns
+        -------
+        NdimInterpolation instance.
+
+        Example
+        -------
+        General way to use ndiminterpolation
+
+          ipython --pylab
+          In[0]: import ndiminterpolation as nd
+          In[1]: ip, datacube, theta, mywave = nd.example()
+
+        See also example.py in the same repository.
+        """
+        
+        self.theta = theta   # list of lists of parameter values, unique, in correct order
+
+        shape_ = [len(t) for t in self.theta]
+
+        # determine if data is hypercube or list of 1d arrays
+        if shape_ == data.shape:
+            self.input = 'hypercube'
+            self.data_hypercube = data
+        else:
+            self.input = 'linear'
+            self.data_hypercube = data.reshape(shape_,order='F')
+
+        # interpolation orders
+        assert (order in (1,3)), "Interpolation spline order not supported! Must be 1 (linear) or 3 (cubic)."
+        self.order = order
+
+        # interpolate in log10 space?
+        self.mode = mode
+        if self.mode == 'log':
+            try:
+                self.data_hypercube = N.log10(self.data_hypercube)
+            except RuntimeWarning:
+                raise Exception, "For mode='log' all entries in 'data' must be > 0."
+
+        # set up n 1-d linear interpolators for all n parameters in theta
+        self.ips = []   # list of 1-d interpolator objects
+        for t in self.theta:
+            self.ips.append(interpolate.interp1d(t,N.linspace(0.,float(t.size-1.),t.size)))
+
+        if self.order == 3:
+            print "Evaluating cubic spline coefficients for subsequent use, please wait..."
+            self.coeffs = ndimage.spline_filter(self.data_hypercube,order=3)
+            print "Done."
+
+
+    def get_coords(self,vec):
+        
+        """Construct a full 2D matrix of coordinates in pixel-space from a
+           vector of coordinates in real space.
+        
+        Parameters:
+        -----------
+
+        vec : tuple
+
+            Tuple of lenght len(self.theta), with each element either
+            a single value of theta_i (of the i-th parameter), or
+            itself a tuple (of arbitrary length). If an element of vec
+            is itself a tuple (of length m), then m*(n-1) combinations
+            of all parameters will be added to the resulting 2D matrix
+            of coordinates.
+
+        Returns:
+        --------
+        coorinates_pix : 2D array
+            2D array of coordinates in pixel space, on which then the
+            multi-dim interpolation can be performed.
+
+            Overall, A = N.prod([len(vec_i) for vec_i in vec])
+            coordinate sets, for B = len(self.theta) parameters, will
+            be generated, i.e. the returned coordinate matrix has
+            coordinates.shape = (A,B).
+
+        shape_ : tuple
+           The shape tuple to reshape coords_pix with to obtain a
+           properly shaped interpolated array.
+
+        Example:
+        --------
+        self.parameters
+            array(['a', 'b', 'c'])
+
+        [t.size for t in self.theta]
+            (3,5,2)
+
+        self.theta
+            [array(1.,2.,3.,), array(10.,15,18,24,26), array(100.,126)]
+
+        # vector of real-space coordinate to interpolate self.data_hypercube on
+        vec = (1.5,18.,110.)
+        
+        # compute pixel-space vector matrix, and shape of resulting array
+        coords_pix, shape_ = self.get_coords(vec)
+
+
+
+        Old example, rework it:
+
+        vec = (0,1,2,3,(0,1,2),(3,4,5),6)
+        vectup = [e if isinstance(e,tuple) else (e,) for e in vec]
+           [(0,), (1,), (2,), (3,), (0, 1, 2), (3, 4, 5), (6,)]
+        coords = N.array([e for e in itertools.product(*vectup)])  # in pixel space
+           array([[0, 1, 2, 3, 0, 3, 6],
+                  [0, 1, 2, 3, 0, 4, 6],
+                  [0, 1, 2, 3, 0, 5, 6],
+                  [0, 1, 2, 3, 1, 3, 6],
+                  [0, 1, 2, 3, 1, 4, 6],
+                  [0, 1, 2, 3, 1, 5, 6],
+                  [0, 1, 2, 3, 2, 3, 6],
+                  [0, 1, 2, 3, 2, 4, 6],
+                  [0, 1, 2, 3, 2, 5, 6]])
+
+        """
+
+#        print "vec = ", vec
+
+        vectup = [e if isinstance(e,tuple) else (e,) for e in vec]
+#        print "vectup = ", vectup
+        shape_ = [len(e) for e in vectup]
+
+        coords_real = N.array([e for e in itertools.product(*vectup)])  # use ip(.) here
+#        print "coords_pix, coords_pix.shape = ", coords_pix, coords_pix.shape
+        
+        columns = coords_real.T.tolist()
+#        print "columns, len(columns) = ", columns, len(columns)
+        
+        coords_pix = N.array([ self.ips[j](columns[j]) for j in xrange(len(columns)) ])
+#        print "coords_pix.shape = ", coords_pix.shape
+#        print "self.data_hypercube.shape = ", self.data_hypercube.shape
+        
+        return coords_pix, shape_
+
+
+    def __call__(self,vector):
+        """Interpolate in N dimensions, using mapping to image coordinates."""
+
+        if self.order == 1:
+            coords, shape_ = self.get_coords(vector)
+            aux = ndimage.map_coordinates(self.data_hypercube,coords,order=1)
+            aux = aux.reshape(shape_)
+# temporarily disabled order==3, b/c not yet tested
+#        elif self.order == 3:
+#            aux = ndimage.map_coordinates(self.coeffs,self.get_coords(vector,pivots=pivots),order=3,prefilter=False)
+
+        if self.mode == 'log':
+            aux = 10.**aux
+
+        return aux
